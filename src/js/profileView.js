285db35d8ab7d8ec2bceac6d6d3b500f097e6720
Thiago Mendes <thiagomendes.dev@gmail.com>
2026-06-12 18:09:01 -0300

    Novas funcionalidades

--- src/js/profileView.js
+++ src/js/profileView.js
@@ -1,4 +1,19 @@
-export function renderProfile(userData, container) {
+export function renderProfile(userData, userRepos, container) {
+
+  const repositoriesHTML = userRepos && userRepos.length > 0 ? userRepos.map(repo => `
+    <a href="${repo.html_url}" target="_blank">
+        <div class="repository-card">    
+            <h3>${repo.name}</h3>
+            <div class="repository-stats">
+                <span>⭐Stars: ${repo.stargazers_count}</span>
+                <span>🍴 Forks: ${repo.forks_count}</span>
+                <span>👀 Watchers: ${repo.watchers_count}</span>
+                <span>💻 Language: ${repo.language || 'Não informada'}</span>
+            </div>
+        </div>
+    </a>
+    `).join('') : `<p>Nenhum repositório encontrado.</p>`;
+
   container.innerHTML = `
     <div class="profile-card">
       <img src="${userData.avatar_url}" alt="Avatar de ${
@@ -18,6 +33,13 @@ export function renderProfile(userData, container) {
         <div class="following">
             <h4>👥 Seguindo</h4>
             <span>${userData.following}</span>
+        </div>
+    </div>
+
+    <div class="profile-repositories">
+        <h2>Repositórios</h2>
+        <div class="repositories">
+            ${repositoriesHTML}
         </div>
     </div>
   `;
